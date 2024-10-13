0fadd9eebb2b30c0478dd07d504362e17b6839c0
Sibusiso Gumede <sibusisojgumede@gmail.com>
2024-10-13 22:10:47 +0200

    Created a hero section and the theme toggle functionality.

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,57 @@
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/hero-img.png';
+import moon from '../../assets/moon.svg';
+import sun from '../../assets/sun.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import githubLight from '../../assets/github-light.svg';
+import { useTheme } from '../../common/ThemeContext';
+
+function Hero() {
+    const { theme, toggleTheme } = useTheme();
+    const themeIcon = theme === 'dark' ? moon : sun;
+    const linkedinIcon = theme === 'dark' ? linkedinDark : linkedinLight;
+    const githubIcon = theme === 'dark' ? githubDark : githubLight;
+
+    return (
+        <section id="hero" className={styles.container}>
+            <div className={styles.colorModeContainer}>
+                <img 
+                    className={styles.hero} 
+                    src={heroImg} 
+                    alt="Profile picture of Sibusiso Gumede"
+                />
+                <img 
+                    className={styles.colorMode}
+                    src={themeIcon} 
+                    alt="Color mode icon"
+                    onClick={toggleTheme}
+                />
+            </div>
+            <div className={styles.info}>
+                <h1>
+                    Sibusiso
+                    <br />
+                    Gumede
+                </h1>
+                <h2>Software Developer</h2>
+                <span>
+                    <a href="https://www.linkedin.com/in/sibusiso-j-gumede-6580b01a8" target="_blank">
+                        <img src={linkedinIcon} alt="Linkedin icon" />
+                    </a>
+                    <a href="https://github.com/Sibusiso-Gumede" target="_blank">
+                        <img src={githubIcon} alt="Github icon" />
+                    </a>
+                </span>
+                <p className={styles.description}>
+                    2 years experience in developing web and mobile applications.
+                    A tinkerer and a builder who is passionate about software systems research and development.
+                    I possess transferable technical experience and can learn new technologies.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default Hero;
