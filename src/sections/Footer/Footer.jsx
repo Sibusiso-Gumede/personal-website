939c70edbdb80c390c2a5d7a10712d58a29b32e2
Sibusiso Gumede <sibusisojgumede@gmail.com>
2024-10-14 21:54:26 +0200

    Added footer section to the layout.

---
+++ src/sections/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import styles from './FooterStyle.module.css'
+
+function Footer() {
+    return (
+        <section id='footer' className={styles.container}>
+            <p>
+                &copy; 2024 Sibusiso J. Gumede. <br />
+                All rights reserved.
+            </p>
+        </section>
+    )
+}
+
+export default Footer
