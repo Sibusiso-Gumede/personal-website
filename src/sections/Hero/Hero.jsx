fab4dc757282fcc92a011785c6a5d25c4bc35e51
Sibusiso Gumede <sibusisojgumede@gmail.com>
2024-10-19 14:42:14 +0200

    Added detail to the description in the hero section of the page layout.

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -46,7 +46,7 @@ function Hero() {
                 </span>
                 <p className={styles.description}>
                     2 years experience in developing web and mobile applications.
-                    Well-versed in Django, Flutter and React.
+                    Equipped with a diverse skill set. Proficient in various platforms, frameworks and languages.
                     I possess transferable technical experience and can learn new technologies.
                 </p>
             </div>
